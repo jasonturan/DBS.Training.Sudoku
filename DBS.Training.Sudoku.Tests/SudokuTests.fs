32dc37e0242f463e8479f77d366546996fc02bb8
Jason Turan <turan.jason@gmail.com>
2017-05-29 21:33:28 -0500

    Adding unit tests

---
+++ DBS.Training.Sudoku.Tests/SudokuTests.fs
@@ -0,0 +1,10 @@
+﻿namespace DBS.Training.Sudoku.Tests
+namespace Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type SudokuTests() = 
+
+    [<TestMethod>]
+    member this.ShouldBeAbleToCreateStuff() = 
+        Assert.AreEqual(2, 2)
+    
